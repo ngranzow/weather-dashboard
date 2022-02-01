65ead11154068103b7f73208693c3f62b14e4002
ngranzow <nate.granzow@gmail.com>
2022-02-01 01:55:27 -0600

    html setup, started css and js

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+var textarea = document.getElementById("textarea");
+var searchBtn = document.getElementById("search-btn");
+var previousBtn = document.getElementById("previous-search-buttons");
+var currentWeatherEl = document.getElementById("current-weather");
+var fiveDayEl = document.getElementById("five-day");
+
