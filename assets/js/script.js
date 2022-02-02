8ebe2cbab2d929c5ff6993c29ce76b934cbdc4d6
ngranzow <nate.granzow@gmail.com>
2022-02-02 09:10:19 -0600

    testing api

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,4 +3,6 @@ var searchBtn = document.getElementById("search-btn");
 var previousBtn = document.getElementById("previous-search-buttons");
 var currentWeatherEl = document.getElementById("current-weather");
 var fiveDayEl = document.getElementById("five-day");
+var mykey = config.MY_KEY;
+
 
